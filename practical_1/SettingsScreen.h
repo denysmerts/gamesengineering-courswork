58ab0c7f2184f36dc73430d0998cf5222f623c35
Cheema <ibrahimcheema0911@gmail.com>
2024-12-12 03:47:57 +0000

    Resolution Setting added

--- practical_1/SettingsScreen.h
+++ practical_1/SettingsScreen.h
@@ -4,29 +4,41 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Window.hpp>
 #include <string>
+#include <vector>
 
 using namespace std;
 using namespace sf;
 
 class SettingsScreen {
 private:
-    Text fullscreenText;
-    Text fullscreenLeftArrow;
-    Text fullscreenRightArrow;
-    Text backText;
+    Text fullscreenText, resolutionText, backText;
     Font& font;
     bool& isFullscreen;
+    VideoMode& videoMode;
+    vector<VideoMode> resolutions = {
+        VideoMode(800, 600),
+        VideoMode(1024, 768),
+        VideoMode(1280, 720),
+        VideoMode(1920, 1080)
+    };
+    int currentResolutionIndex = 3;
+    RenderWindow& window; // Added this line
+
+    void updateResolutionText();
 
 public:
-    SettingsScreen(Font& font, bool& fullscreenFlag);
+    SettingsScreen(Font& font, bool& fullscreenFlag, VideoMode& videoMode, RenderWindow& window);
 
     void handleEvent(Event& event, RenderWindow& window, bool& backToStartScreen);
     void render(RenderWindow& window);
+    SettingsScreen(Font& font, bool& fullscreenFlag);
 
 private:
     Text createText(const string& content, Font& font, unsigned int size, Color color);
     void centerText(Text& text, const VideoMode& mode);
+    sf::Text fullscreenLeftArrow;
+    sf::Text fullscreenRightArrow;
     void applyResolution(RenderWindow& window, VideoMode& videoMode, bool isFullscreen);
 };
 
-#endif 
+#endif
